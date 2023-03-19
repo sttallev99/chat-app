3d421f4ab96b65fe9661de2d6dbe6160481ed48a
Георги Сталев <sttallev99@gmail.com>
2023-03-19 18:09:55 +0200

    fetch privete chat messages

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,7 @@
 import { doc, onSnapshot } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthConntext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 import { db } from '../firebase';
 
 import profilePicture from '../img/profilePicture.jpg'
@@ -10,6 +11,7 @@ const Chats = () => {
   const [chats, setChats] = useState([]);
 
   const {currentUser} = useContext(AuthConntext);
+  const {dispatch} = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -25,10 +27,14 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
+  const handleSelect = (u) => {
+    dispatch({type: "CHANGE_USER", payload: u})
+  }
+
   return (
     <div className='chats'>
         {Object.entries(chats)?.map(chat => (
-          <div className="userChat" key={chat[0]}>
+          <div className="userChat" key={chat[0]} onClick = {() => handleSelect(chat[1].userInfo)}>
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthConntext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext';
 
 import ProfilePicture from '../img/profilePicture.jpg'
 
-const Message = () => {
+const Message = ({message}) => {
+  const {currentUser} = useContext(AuthConntext);
+  const {data} = useContext(ChatContext);
+
+  console.log(message)
   return (
     <div className='message owner'>
-      <div className="messageInfo">
+      {/* <div className="messageInfo">
         <img src={ProfilePicture} alt="" />
         <span>Just now</span>
       </div>
       <div className="messageContent">
         <p>Hello</p>
         <img src={ProfilePicture} alt="" />
-      </div>
+      </div> */}
     </div>
   )
 }
